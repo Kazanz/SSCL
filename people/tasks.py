b38a020fc910f36efa534afc7569dc6e66122db6
Zach Kazanski <kazanski.zachary@gmail.com>
2016-07-05 00:28:40 -0400

    feat(nexmo): send texts with nexmo

--- people/tasks.py
+++ people/tasks.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import nexmo
 import requests
 from celery import shared_task
 from django.conf import settings
@@ -19,7 +20,7 @@ def send_msg(subject, body=None, txtbody=None, withlink=True):
             send_with_mailgun(waiver.email, subject, msg)
         if txtbody:
             textmsg = make_msg(txtbody, waiver.hash)
-            send_with_mailgun(waiver.number, subject, textmsg)
+            send_with_nexmo(waiver.phone, subject, textmsg)
         waiver.sent = datetime.now()
         waiver.save()
 
@@ -37,3 +38,14 @@ def send_with_mailgun(to, subject, msg):
               "to": to,
               "subject": subject,
               "text": msg})
+
+
+def send_with_nexmo(number, subject, msg):
+    if settings.DEBUG:
+        number = "8133893559"
+    client = nexmo.Client(key=settings.NEXMO_KEY, secret=settings.NEXMO_SECRET)
+    client.send_message({
+        'from': '12013508725',
+        'to': "1" + number,
+        'text': "{} - {}".format(subject, msg)
+    })
